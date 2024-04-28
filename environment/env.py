68c371d92d12523f29bf00e2731b21177eecec38
ErnestBeckham <ernestbeckham1998@gmail.com>
2024-04-28 14:34:36 +0530

    env completed

--- environment/env.py
+++ environment/env.py
@@ -49,7 +49,7 @@ class Environment:
         pygame.draw.rect(screen, wall_color, left_wall)
         pygame.draw.rect(screen, wall_color, right_wall)
 
-    def T_component(self, pos:list, angle, screen):
+    def T_component(self, pos, angle, size, screen):
         # Define the color for the T component
         t_color = (66, 106, 166)  # Blue color
 
@@ -58,24 +58,30 @@ class Environment:
 
         # Define the coordinates for the T component
         x, y = pos
-        
-        if angle == 90:
-            horizontal_bar = pygame.Rect(x, y, t_thickness, 100)
-            vertical_bar = pygame.Rect(x, y+50, 100, t_thickness)
-        elif angle == 180:
-            horizontal_bar = pygame.Rect(x, y, 100, t_thickness)
-            vertical_bar = pygame.Rect(x + 50 - (t_thickness // 2), y , t_thickness, 100)
-        elif angle == 270:
-            horizontal_bar = pygame.Rect(x, y + 50, t_thickness, 100)
-            vertical_bar = pygame.Rect(x-100, y+100, 100, t_thickness)
-        elif angle == None:
-            horizontal_bar = pygame.Rect(x, y, 100, t_thickness)
-            vertical_bar = pygame.Rect(x + 50 - (t_thickness // 2), y, t_thickness, 100)
 
+        # Adjust the size of the T component
+        t_width = size
+        t_height = size * 2
+
+        # Draw the T component based on the angle
+        if angle == 90:
+            horizontal_bar = pygame.Rect(x, y, t_thickness, t_height)
+            vertical_bar = pygame.Rect(x, y + t_height//2, t_width, t_thickness)
+        elif angle == 180:
+            horizontal_bar = pygame.Rect(x, y, t_width, t_thickness)
+            vertical_bar = pygame.Rect(x + t_width//2 - (t_thickness // 2), y, t_thickness, t_height)
+        elif angle == 270:
+            horizontal_bar = pygame.Rect(x, y + t_height, t_thickness, t_height)
+            vertical_bar = pygame.Rect(x - t_width//2, y + t_height, t_width, t_thickness)
+        elif angle == 360:
+            # Draw the T component at 360 degrees (same as 90 degrees)
+            horizontal_bar = pygame.Rect(x, y, t_width, t_thickness)
+            vertical_bar = pygame.Rect(x + t_width//2 - (t_thickness // 2), y - t_height, t_thickness, t_height)
 
         # Draw the T component on the screen
         pygame.draw.rect(screen, t_color, horizontal_bar)
         pygame.draw.rect(screen, t_color, vertical_bar)
+
 
     def box(self, size:list, pos:list, screen):
         box_color = (66, 106, 166)  # Blue color
@@ -104,7 +110,7 @@ class Environment:
         line_color = (66, 106, 166)  # Blue color
 
         # Define the thickness of the line
-        line_thickness = 10
+        line_thickness = 5
 
         # Define the coordinates for the line
         x, y = pos
@@ -117,7 +123,7 @@ class Environment:
             pygame.draw.line(screen, line_color, (x, y), (x, y + length), line_thickness)
 
     
-    def L_component(self, size:int, pos:list, angle:int, screen):
+    def L_component(self, size:int, pos:list, angle:int, flip:bool, screen):
         l_color = (66, 106, 166)  # Blue color
 
         # Define the thickness of the L component
@@ -127,17 +133,41 @@ class Environment:
         x, y = pos
         
         if angle == 90:
-            # Draw the L component at 90 degrees
-            pygame.draw.rect(screen, l_color, pygame.Rect(x, y, l_thickness, size), 0)  # Vertical bar
-            pygame.draw.rect(screen, l_color, pygame.Rect(x, y + size - l_thickness, size, l_thickness), 0)  # Horizontal bar
+            if not flip:
+                # Draw the L component at 90 degrees (normal)
+                pygame.draw.rect(screen, l_color, pygame.Rect(x, y, l_thickness, size), 0)  # Vertical bar
+                pygame.draw.rect(screen, l_color, pygame.Rect(x, y + size - l_thickness, size, l_thickness), 0)  # Horizontal bar
+            else:
+                # Draw the L component at 90 degrees (flipped)
+                pygame.draw.rect(screen, l_color, pygame.Rect(x, y + size - l_thickness, size, l_thickness), 0)  # Horizontal bar
+                pygame.draw.rect(screen, l_color, pygame.Rect(x + size - l_thickness, y, l_thickness, size), 0)  # Vertical bar
         elif angle == 180:
-            # Draw the L component at 180 degrees
-            pygame.draw.rect(screen, l_color, pygame.Rect(x, y, size, l_thickness), 0)  # Horizontal bar
-            pygame.draw.rect(screen, l_color, pygame.Rect(x + size - l_thickness, y, l_thickness, size), 0)  # Vertical bar
+            if not flip:
+                # Draw the L component at 180 degrees (normal)
+                pygame.draw.rect(screen, l_color, pygame.Rect(x, y, size, l_thickness), 0)  # Horizontal bar
+                pygame.draw.rect(screen, l_color, pygame.Rect(x + size - l_thickness, y, l_thickness, size), 0)  # Vertical bar
+            else:
+                # Draw the L component at 180 degrees (flipped)
+                pygame.draw.rect(screen, l_color, pygame.Rect(x, y, size, l_thickness), 0)  # Horizontal bar
+                pygame.draw.rect(screen, l_color, pygame.Rect(x, y, l_thickness, size), 0)  # Vertical bar
         elif angle == 270:
-            # Draw the L component at 270 degrees
-            pygame.draw.rect(screen, l_color, pygame.Rect(x, y, l_thickness, size), 0)  # Vertical bar
-            pygame.draw.rect(screen, l_color, pygame.Rect(x, y, size, l_thickness), 0)
+            if not flip:
+                # Draw the L component at 270 degrees (normal)
+                pygame.draw.rect(screen, l_color, pygame.Rect(x, y, l_thickness, size), 0)  # Vertical bar
+                pygame.draw.rect(screen, l_color, pygame.Rect(x, y, size, l_thickness), 0)
+            else:
+                # Draw the L component at 270 degrees (flipped)
+                pygame.draw.rect(screen, l_color, pygame.Rect(x, y, l_thickness, size), 0)  # Vertical bar
+                pygame.draw.rect(screen, l_color, pygame.Rect(x, y + size - l_thickness, size, l_thickness), 0)  # Horizontal bar
+        elif angle == 360:
+            if not flip:
+                # Draw the L component at 360 degrees (normal)
+                pygame.draw.rect(screen, l_color, pygame.Rect(x, y, l_thickness, size), 0)  # Vertical bar
+                pygame.draw.rect(screen, l_color, pygame.Rect(x, y, size, l_thickness), 0)  # Horizontal bar
+            else:
+                # Draw the L component at 360 degrees (flipped, same as 90 degrees)
+                pygame.draw.rect(screen, l_color, pygame.Rect(x, y, l_thickness, size), 0)  # Vertical bar
+                pygame.draw.rect(screen, l_color, pygame.Rect(x, y, size, l_thickness), 0)  # Horizontal bar
 
 
     def enemy_home(self, screen):
@@ -170,3 +200,51 @@ class Environment:
         pygame.draw.line(screen,home_color, (795, 400), (850, 400), line_thickness)
 
     
+
+    def draw_environment(self, screen):
+        self.outline(screen)
+        self.outline_rotated(screen)
+
+        #left side
+        #up
+        self.create_line(100, "vertical", [250, 8], screen)
+        self.create_line(120, "horizontal", [8, 175], screen)
+        self.L_component(240, [8, 170], 90, flip=True, screen=screen)
+        
+        # down
+        self.create_line(100, "vertical", [250, 900-108], screen)
+        self.create_line(120, "horizontal", [8, 725], screen)
+        self.L_component(240, [8, 500], 180, flip=False, screen=screen)
+
+        ################################################################################################################################
+
+        #right side
+        #up
+        self.create_line(100, "vertical", [1250, 8], screen)
+        self.create_line(120, "horizontal", [1500-128, 175], screen)
+        self.L_component(240, [1500-248, 170], 90, flip=False, screen=screen)
+
+        #down
+        self.create_line(100, "vertical", [1250, 900-108], screen)
+        self.create_line(120, "horizontal", [1500-128, 725], screen)
+        self.L_component(240, [1500-248, 500], 180, flip=True, screen=screen)
+
+        self.enemy_home(screen)
+
+        self.T_component([500, 900-100], angle=360, size=80, screen=screen)
+        self.T_component([1000, 900-100], angle=360, size=80, screen=screen)
+
+        self.T_component([530, -50], angle=270, size=80, screen=screen)
+        self.T_component([1030, -50], angle=270, size=80, screen=screen)
+
+        self.box(size=[150,80], pos=[710, 670], screen=screen)
+        self.box(size=[150,80], pos=[710, 150], screen=screen)
+
+
+        self.create_line(220, "vertical", [400, 355], screen=screen)
+        self.create_line(100, "horizontal", [400, 355], screen=screen)
+        self.create_line(100, "horizontal", [400, 575], screen=screen)
+
+        self.create_line(220, "vertical", [1150, 355], screen=screen)
+        self.create_line(100, "horizontal", [1050, 355], screen=screen)
+        self.create_line(100, "horizontal", [1050, 575], screen=screen)
